77bc5168dd10b184ae94d1e99561de81a50e43e5
Lautaro Terreno <95369655+lauterre@users.noreply.github.com>
2023-10-27 16:31:09 -0300

    ordenamiento en clase

---
+++ TP_Final/columna/ColumnaBoolean.java
@@ -0,0 +1,103 @@
+package columna;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import celda.Celda;
+import celda.CeldaBoolean;
+
+public class ColumnaBoolean extends Columna {
+    private List<CeldaBoolean> celdas;
+
+    public ColumnaBoolean(List<CeldaBoolean> celdas) {
+        this.celdas = celdas;
+    }
+
+    @Override
+    public void ordenar(String orden){
+        Collections.sort(celdas);
+
+        if ("descendente".equals(orden)) {
+            Collections.reverse(celdas);
+        }
+    }
+
+    @Override
+    public Celda obtenerValor(Integer indiceFila){
+        // Asegúrate de que el índice de fila sea válido
+        if (indiceFila >= 0 && indiceFila < celdas.size()) {
+            return celdas.get(indiceFila); //celdas.get(indiceFila).getValor()?
+        } else {
+            throw new IllegalArgumentException("Índice de fila fuera de rango");
+        }
+    }
+
+    @Override
+    public void fijarValor(Integer indiceFIla, Celda valor) {
+        // Asegúrate de que el índice de fila sea válido
+        if (indiceFIla >= 0 && indiceFIla < celdas.size()) {
+            celdas.set(indiceFIla, (CeldaBoolean) valor);
+        } else {
+            throw new IllegalArgumentException("Índice de fila fuera de rango");
+        }
+    }
+
+    @Override
+    public String tipoDato() {
+        return "Booleana".toString();
+    }
+
+    @Override
+    public int size() {
+        return celdas.size();
+    }
+
+    public List<CeldaBoolean> getCeldas() {
+        return celdas;
+    }
+
+    public void setCeldas(List<CeldaBoolean> celdas) {
+        this.celdas = celdas;
+    }
+
+    //para probar cosas:
+    public String toString() {
+        String inicial = "";
+        for (CeldaBoolean celda : celdas) {
+            inicial += celda.getValor();
+            inicial += ", ";
+        }
+        return inicial;
+    }
+
+    public static void main(String[] args) {
+        CeldaBoolean celda1 = new CeldaBoolean(true);
+        CeldaBoolean celda2 = new CeldaBoolean(false);
+        CeldaBoolean celda3 = new CeldaBoolean(1);
+        CeldaBoolean celda4 = new CeldaBoolean(0);
+        CeldaBoolean celda5 = new CeldaBoolean((Integer) null);
+        CeldaBoolean celda6 = new CeldaBoolean((Boolean)null);
+
+        List<CeldaBoolean> listaCeldas = new ArrayList<>();
+        listaCeldas.add(celda1);
+        listaCeldas.add(celda2);
+        listaCeldas.add(celda3);
+        listaCeldas.add(celda4);
+        listaCeldas.add(celda5);
+        listaCeldas.add(celda6);
+
+        ColumnaBoolean col = new ColumnaBoolean(listaCeldas);
+
+        System.out.println("original: " + col);
+
+        col.ordenar("descendente");
+
+        System.out.println("desc: "+ col);
+
+        col.ordenar(null);
+
+        System.out.println("asc: " + col);
+
+    }
+}
---
+++ TP_Final/columna/ColumnaString.java
@@ -0,0 +1,103 @@
+package columna;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import celda.Celda;
+import celda.CeldaString;
+
+public class ColumnaString extends Columna{
+    private List<CeldaString> celdas;
+
+    public ColumnaString(List<CeldaString> celdas) {
+        this.celdas = celdas;
+    }
+
+    @Override
+    public void ordenar(String orden){
+        Collections.sort(celdas);
+
+        if ("descendente".equals(orden)) {
+            Collections.reverse(celdas);
+        }
+    }
+
+    @Override
+    public Celda obtenerValor(Integer indiceFila){
+        // Asegúrate de que el índice de fila sea válido
+        if (indiceFila >= 0 && indiceFila < celdas.size()) {
+            return celdas.get(indiceFila); //celdas.get(indiceFila).getValor()?
+        } else {
+            throw new IllegalArgumentException("Índice de fila fuera de rango");
+        }
+    }
+
+    @Override
+    public void fijarValor(Integer indiceFIla, Celda valor) {
+        // Asegúrate de que el índice de fila sea válido
+        if (indiceFIla >= 0 && indiceFIla < celdas.size()) {
+            celdas.set(indiceFIla, (CeldaString) valor);
+        } else {
+            throw new IllegalArgumentException("Índice de fila fuera de rango");
+        }
+    }
+
+    @Override
+    public String tipoDato() {
+        return "String".toString();
+    }
+
+    @Override
+    public int size() {
+        return celdas.size();
+    }
+
+    public List<CeldaString> getCeldas() {
+        return celdas;
+    }
+
+    public void setCeldas(List<CeldaString> celdas) {
+        this.celdas = celdas;
+    }
+
+    //para probar cosas:
+    public String toString() {
+        String inicial = "";
+        for (CeldaString celda : celdas) {
+            inicial += celda.getValor();
+            inicial += ", ";
+        }
+        return inicial;
+    }
+
+    public static void main(String[] args) {
+        CeldaString celda1 = new CeldaString("c");
+        CeldaString celda2 = new CeldaString("a");
+        CeldaString celda3 = new CeldaString("d");
+        CeldaString celda4 = new CeldaString("NA");
+        CeldaString celda5 = new CeldaString(null);
+        CeldaString celda6 = new CeldaString("na");
+
+        List<CeldaString> listaCeldas = new ArrayList<>();
+        listaCeldas.add(celda1);
+        listaCeldas.add(celda2);
+        listaCeldas.add(celda3);
+        listaCeldas.add(celda4);
+        listaCeldas.add(celda5);
+        listaCeldas.add(celda6);
+
+        ColumnaString col = new ColumnaString(listaCeldas);
+
+        System.out.println("original: " + col);
+
+        col.ordenar("descendente");
+
+        System.out.println("desc: "+ col);
+
+        col.ordenar(null);
+
+        System.out.println("asc: " + col);
+
+    }
+}
