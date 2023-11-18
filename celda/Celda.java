13f7ee83a67640f19d720641d17ef520948f513f
nazarenomm <nazarenomm98@gmail.com>
2023-11-18 13:31:57 -0300

    agregarColumna

--- celda/Celda.java
+++ celda/Celda.java
@@ -1,5 +1,8 @@
 package celda;
 
+import etiqueta.EtiquetaNum;
+import etiqueta.EtiquetaString;
+
 public abstract class Celda implements Comparable<Celda>{
 
     public abstract boolean isNA();
@@ -10,4 +13,16 @@ public abstract class Celda implements Comparable<Celda>{
     public abstract int compareTo(Celda o);
 
     public abstract Celda copia();
+
+    public static CeldaNum crear(int e) {
+        return new CeldaNum(e);
+    }
+
+    public static CeldaString crear(String e) {
+        return new CeldaString(e);
+    }
+
+    public static CeldaBoolean crear(boolean e) {
+        return new CeldaBoolean(e);
+    }
 }
